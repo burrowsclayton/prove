c176aaa206dd8790df102f57521df618ab430236
Tripodi <trije@santos.com>
2018-01-30 14:35:31 +1030

    Updates code documentation and spelling errors #17

--- scripts/scatter_plot.py
+++ scripts/scatter_plot.py
@@ -1,30 +1,30 @@
+""""
+Author: Joseph Tripodi
+Contact: au.joseph.tripodi@gmail.com
+For more information see: https://github.com/joseph-tripodi/prove/wiki/04.-Spotfire-API#spotfire-api-v76
+""""
+
 from Spotfire.Dxp.Application.Visuals import *
 from Spotfire.Dxp.Data import *
 
-# Getting the data table by searching all data tables and getting the name
-# of the first data table in the list, the data table is the data imported from
-# the spreadsheet or database
+# Get a handle on the data table (object that contains all data)
 data_table_handle = Document.Data.Tables
-# Tablename is the name of the first table found
 data_table_name = [table.Name for table in data_table_handle][0]
-# Selecting the data table, based on the table name found
 data_table_handle = data_table_handle[ data_table_name ]
 
 # Creating the scatter plot
 scatter_plot_handle = Document.ActivePageReference.Visuals.AddNew[ScatterPlot]()
 # Linking the data table with the newly created scatter plot
 scatter_plot_handle.Data.DataTableReference = data_table_handle
-# Setiing the y axis to plot the column GasRate from the excel spreadsheet
+
+# Setting the x and y axis of the scatter plot, and setting them to use a log scale
 scatter_plot_handle.YAxis.Expression = "[GAS_RATE]"
-# Setiing the x axis to plot the column Number of days produced from the excel spreadsheet
 scatter_plot_handle.XAxis.Expression = "[NUMBER_OF_DAYS_PRODUCED]"
-# Setting the title of the scatter plot
-scatter_plot_handle.Title = data_table_name
-# Use Log scale for box x and y axis
 scatter_plot_handle.XAxis.UseLogTransform = True
 scatter_plot_handle.YAxis.UseLogTransform = True
 
-#Change shape to circles
+# Display settings of the scatter plot
+scatter_plot_handle.Title = data_table_name
 scatter_plot_handle.ShapeAxis.DefaultShape = MarkerShape(MarkerType.Circle)
 
 print "Script Complete"
