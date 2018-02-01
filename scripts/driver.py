be36aa797d45433d57e5b936830aa05fde165c88
Tripodi <trije@santos.com>
2018-02-01 12:02:12 +1030

    Splits code into 3 files Driver: This runs the entire application, by calling the scatter plot script as well as the fetkovich script Scatter Plot: This produces the scatter plot Fetkovich: This attempts to create the simplified fetkovich curve #36

--- scripts/driver.py
+++ scripts/driver.py
@@ -11,3 +11,14 @@ This script runs the entire project.
 
 from Spotfire.Dxp.Application.Visuals import *
 from Spotfire.Dxp.Data import *
+from Spotfire.Dxp.Application.Scripting import ScriptDefinition
+import clr
+
+scatter_plot = clr.Reference[ScriptDefinition]()
+simplified_fetkovich = clr.Reference[ScriptDefinition]()
+Document.ScriptManager.TryGetScript("scatter_plot", scatter_plot)
+Document.ScriptManager.TryGetScript("simplified_fetkovich", simplified_fetkovich)
+
+Document.ScriptManager.ExecuteScript(scatter_plot.ScriptCode, {})
+
+
--- scripts/simplified_fetkovich.py
+++ scripts/simplified_fetkovich.py
@@ -11,3 +11,35 @@ Plots the simplified fetkovich curve
 
 from Spotfire.Dxp.Application.Visuals import *
 from Spotfire.Dxp.Data import *
+
+# Get the data table (object that contains all data)
+data_table = Document.Data.Tables
+data_table_name = [table.Name for table in data_table][0]
+data_table = data_table[ data_table_name ]
+
+# Getting a reference to the scatter plot
+scatter_plot = None
+for visual in Document.ActivePageReference.Visuals:
+  if "ScatterPlot" in str(visual.TypeId):
+    scatter_plot = visual
+
+# Selecting the filtered rows for line calculations
+# Creating the cursors to use in our column selections
+columns = data_table.Columns
+date_cursor = DataValueCursor.Create(columns["END_OF_MONTH_DATE"])
+slope_cursor = DataValueCursor.Create(columns["SLOPE"])
+gas_rate_cursor = DataValueCursor.Create(columns["GAS_RATE_MSCF_PD"])
+days_cursor = DataValueCursor.Create(columns["NUMBER_OF_DAYS_PRODUCED"])
+
+# Creating a row selection that allows us to only get the rows that match
+# the current filtering options
+filtering = Document.ActiveFilteringSelectionReference
+# The selection needs to be converted to an Enumerable type to be used in GetRows
+row_selection = filtering.GetSelection(data_table).AsIndexSet()
+
+slope_hash, gas_rate_hash, day_hash = {}, {}, {}
+
+for each in data_table.GetRows(row_selection, date_cursor, slope_cursor, gas_rate_cursor, days_cursor):
+  slope_hash[date_cursor.CurrentValue] = slope_cursor.CurrentValue
+  gas_rate_hash[date_cursor.CurrentValue] = gas_rate_cursor.CurrentValue
+  day_hash[date_cursor.CurrentValue] = days_cursor.CurrentValue
