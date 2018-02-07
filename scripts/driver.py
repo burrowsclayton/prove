f498f94b5be1db4ab7b5a5b1ea2f129ec25e3402
Tripodi <trije@santos.com>
2018-02-07 13:01:06 +1030

    No longer plots unless a well has been selcted #49 It will no longer attempt to plot unless as well has been selected. Updated gitignore as well (I know I shouldn't have, but I did) Updated spotfire app scripts

--- scripts/driver.py
+++ scripts/driver.py
@@ -13,24 +13,34 @@ from Spotfire.Dxp.Application.Visuals import *
 from Spotfire.Dxp.Data import *
 from Spotfire.Dxp.Application.Scripting import ScriptDefinition
 import clr
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import MessageBox
 
-# Making sure a field and well name has been selected before plotting
+# Getting the data table object
 table_name = "Monthly Production information link"
 data_table = Document.Data.Tables[table_name]
-well_selected = True
 
+# Determines if a well has been selected by looking at the context
+# of the filtering option "WELL_NAME"
+well_selected = True
 for filteringScheme in Document.FilteringSchemes:
   for filter in filteringScheme[data_table]:
     if filter.Name == "WELL_NAME":
       if str(filter.Context) == "WELL_NAME: ((All))":
         well_selected = False
 
+if well_selected:
+  # Produced the prove analysis scatter plot
 
-scatter_plot = clr.Reference[ScriptDefinition]()
-kmeans = clr.Reference[ScriptDefinition]()
+  scatter_plot = clr.Reference[ScriptDefinition]()
+  kmeans = clr.Reference[ScriptDefinition]()
 
-Document.ScriptManager.TryGetScript("scatter_plot", scatter_plot)
-Document.ScriptManager.TryGetScript("kmeans", kmeans)
+  Document.ScriptManager.TryGetScript("scatter_plot", scatter_plot)
+  Document.ScriptManager.TryGetScript("kmeans", kmeans)
 
-Document.ScriptManager.ExecuteScript(scatter_plot.ScriptCode, {})
-Document.ScriptManager.ExecuteScript(kmeans.ScriptCode, {})
+  Document.ScriptManager.ExecuteScript(scatter_plot.ScriptCode, {})
+  Document.ScriptManager.ExecuteScript(kmeans.ScriptCode, {})
+
+else:
+  # If no well has been selected then alert the user
+  MessageBox.Show("Please select a well", "No well selected")
